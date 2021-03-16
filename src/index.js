a8a1ffb35b6253c2bc63c6d586280af655d8c3cf
Joe@Edood <Doode1524@gmail.com>
2021-03-16 19:29:26 -0400

    custom children and component reuse

--- src/index.js
+++ src/index.js
@@ -7,41 +7,50 @@ import ApprovalCard from './ApprovalCard'
 const App = () =>{
     return (
         <div className="ui container comments">
-            <ApprovalCard />
+        <ApprovalCard>
           <CommentDetail 
             author="Bob" 
             timeAgo="Today at 4:45pm" 
             commentText="Hello there!" 
             avatar={faker.image.image()}
             />
+        </ApprovalCard>
 
+        <ApprovalCard> 
           <CommentDetail 
             author="Joey" 
             timeAgo="Today at 2:00am" 
             commentText="Comment 2" 
             avatar={faker.image.image()}
           />
+        </ApprovalCard>
 
+        <ApprovalCard>
           <CommentDetail 
                author="Wes" 
             timeAgo ="Yesterday at 5:23pm" 
             commentText="Comment 3" 
             avatar={faker.image.image()}
           />
+        </ApprovalCard>
 
+        <ApprovalCard>
           <CommentDetail 
             author="Charles" 
             timeAgo ="Yesterday at 5:43pm" 
             commentText="Comment 4" 
             avatar={faker.image.image()}
           />
+        </ApprovalCard>
 
+        <ApprovalCard>
           <CommentDetail 
             author="Enoch"    
             timeAgo ="Yesterday at 6:23pm" 
             commentText="Comment 5" 
             avatar={faker.image.image()}
           />
+        </ApprovalCard>
         </div>
     )
 }
